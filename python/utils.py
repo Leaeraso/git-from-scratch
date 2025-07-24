d1eef955cb5f7d7b95029d595f32522b25a474bb
Leaeraso <leaeraso@gmail.com>
2025-07-23 23:38:18 -0300

    python

---
+++ python/utils.py
@@ -0,0 +1,61 @@
+
+import collections
+import hashlib
+import os
+import struct
+import zlib
+
+
+def write_file(path, content):
+    with open(path, 'wb') as f:
+        f.write(content)
+
+def read_file(path):
+    with open(path, 'rb') as f:
+        return f.read()
+
+def hash_object(data, obj_type, write=True):
+    """Compute hash of object data of given type and write to object store
+    if "write" is True. Return SHA-1 object hash as hex string.
+    """
+    header = '{} {}'.format(obj_type, len(data)).encode()
+    full_data = header + b'\x00' + data
+    sha1 = hashlib.sha1(full_data).hexdigest()
+    if write:
+        path = os.path.join('.git', 'objects', sha1[:2], sha1[2:])
+        if not os.path.exists(path):
+            os.makedirs(os.path.dirname(path), exist_ok=True)
+            write_file(path, zlib.compress(full_data))
+    return sha1
+
+IndexEntry = collections.namedtuple('IndexEntry', [
+    'ctime_s', 'ctime_n', 'mtime_s', 'mtime_n', 'dev', 'ino', 'mode', 'uid', 'gid', 'size', 'sha1', 'flags', 'path'
+])
+
+def read_index():
+    """Read git index file and return list of IndexEntry objects."""
+    try:
+        data = read_file(os.path.join('.git', 'index'))
+    except FileNotFoundError:
+        return []
+    digest = hashlib.sha1(data[:-20]).digest()
+    assert digest == data[-20:], 'invalid index checksum'
+    signature, version, num_entries = struct.unpack('!4sLL', data[:12])
+    assert signature == b'DIRC', \
+        'invalid index signature {}'.format(signature)
+    assert version == 2, 'unknown index version {}'.format(version)
+    entry_data = data[12:-20]
+    entries = []
+    i = 0
+    while i + 62 < len(entry_data):
+        fields_end = i + 62
+        fields = struct.unpack('!LLLLLLLLLL20sH',
+        entry_data[i:fields_end])
+        path_end = entry_data.index(b'\x00', fields_end)
+        path = entry_data[fields_end:path_end]
+        entry = IndexEntry(*(fields + (path.decode(),)))
+        entries.append(entry)
+        entry_len = ((62 + len(path) + 8) // 8) * 8
+        i += entry_len
+    assert i == len(entries) == num_entries
+    return entries
